9739e447de5c8d5b3a431fb3d9878f360375e901
TylerHodges <driftka24de@gmail.com>
2023-07-26 19:57:25 -0500

    added main4

---
+++ main4.py
@@ -0,0 +1,45 @@
+import openai
+import discord
+import keys
+import re
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+# Set up OpenAI API credentials
+openai.api_key = keys.openai_api_key
+
+# Create global chat history variable
+chathistory = [
+    {"role": "system", "content": "You are a Discord Bot that is powered by OpenAI. You are an absolute CHAD. Your name is ChadGPT."},
+]
+
+def chat_completion(message):
+    # Generate a response using OpenAI's GPT-3
+    prompt = message.content
+    regex = r"<.*?>"
+    prompt = re.sub(regex, "", prompt)
+    chathistory.append({"role": "user", "content": prompt})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=chathistory,
+        temperature=0.3,
+    )
+    reply = response.choices[0].message.content
+    chathistory.append({"role": "assistant", "content": reply})
+    return reply
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    # Check if the bot was mentioned or if 'ChadGPT' is in the message content
+    if client.user.mentioned_in(message) or 'ChadGPT' in message.content:
+        reply = chat_completion(message)
+        await message.channel.send(reply)
+
+client.run(keys.DISCORD_TOKEN)
